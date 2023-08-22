8a52c1b86ec31a835499bc8bcc62ecefc5bfd1ec
IvoGeorgievX <ivaylo.georgievxx@gmail.com>
2023-08-22 19:10:29 +0300

    adding features

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -10,3 +10,4 @@ const observer = new IntersectionObserver(entries => {
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((element) => observer.observe(element))
+
