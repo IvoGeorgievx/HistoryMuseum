192140ab243d8c18ae08eaee3ca001dbcfa08818
IvoGeorgievX <ivaylo.georgievxx@gmail.com>
2023-09-05 13:47:41 +0300

    adding features

--- models/user.py
+++ models/user.py
@@ -13,11 +13,19 @@ class User(db.Model):
     role = db.Column(db.Enum(UserRole), nullable=False)
 
 
-class JobApplicant(User):
+class JobApplicant(db.Model):
     __tablename__ = "job_applicant"
     id = db.Column(db.Integer, db.ForeignKey("user.id"), primary_key=True)
+    first_name = db.Column(db.String, nullable=False)
+    last_name = db.Column(db.String, nullable=False)
+    phone_number = db.Column(db.String, nullable=False)
+    age = db.Column(db.Integer, nullable=False)
 
 
-class Company(User):
+class Company(db.Model):
     __tablename__ = "company"
     id = db.Column(db.Integer, db.ForeignKey("user.id"), primary_key=True)
+    company_name = db.Column(db.String, nullable=False)
+    company_address = db.Column(db.String, nullable=False)
+    company_phone_number = db.Column(db.String, nullable=False)
+    company_description = db.Column(db.String, nullable=False)
