9eb70d9da2fc3061c32c88e09e75b513863f3951
IvoGeorgievX <ivaylo.georgievxx@gmail.com>
2023-09-06 20:57:18 +0300

    adding features

--- frontend/static/js/register.js
+++ frontend/static/js/register.js
@@ -1,6 +1,7 @@
 const hiddenJobApplicantDiv = document.querySelector('.job-applicant')
 
 const radioJobApplicant = document.querySelector('.account-type input[id="job-applicant"]')
+radioJobApplicant.disabled = true
 radioJobApplicant.addEventListener('change', () => {
     if (radioJobApplicant.checked) {
         hiddenJobApplicantDiv.style.display = 'flex'
@@ -10,6 +11,7 @@ radioJobApplicant.addEventListener('change', () => {
 
 const hiddenCompanyDiv = document.querySelector('.company')
 const radioCompany = document.querySelector('.account-type input[id="company"]')
+radioCompany.disabled = true
 
 radioCompany.addEventListener('change', () => {
     if (radioCompany.checked) {
@@ -29,41 +31,89 @@ const userInputs = {
 const inputSpans = {
     username: document.querySelector('#username-span'),
     email: document.querySelector('#email-span'),
-    password1: document.querySelector('#password-span'),
-    password2: document.querySelector('#password2-span'),
+    password1: document.querySelector('#password1'),
+    password2: document.querySelector('#passwordTwo'),
 }
 
 const validationRules = {
     username: {
         validator: isValidUsername,
-        message: 'Username must contain only letters and numbers and be at least 8 characters long.'
+        message: 'Username must contain only letters and numbers and be at least 8 characters long.',
+        isValid: false
     },
     email: {
         validator: isValidEmail,
-        message: 'Please enter a correct email address.'
+        message: 'Please enter a correct email address.',
+        isValid: false
+    },
+    password1 : {
+        validator: isValidPassword,
+        message: 'Password must contain only letters and numbers and be at least 8 characters long.',
+        isValid: false
+    },
+    password2 : {
+        isValid: false
     }
 }
 
 const outlineBorders = {
-    correct: '3px solid green',
+    correct: '4px solid green',
     incorrect: '2px solid red'
 }
 Object.keys(userInputs).map(inputName => {
     const input = userInputs[inputName];
-    console.log(userInputs[inputName])
-    input.addEventListener('blur', () => {
+    input.addEventListener('input', () => {
         const inputValue = input.value;
         if (!validationRules[inputName].validator(inputValue)) {
             inputSpans[inputName].innerHTML = validationRules[inputName].message;
-            input.value = '';
             input.style.border = outlineBorders.incorrect;
+            validationRules[inputName].isValid = false;
         } else {
             inputSpans[inputName].innerHTML = '';
             input.style.border = outlineBorders.correct;
+            validationRules[inputName].isValid = true;
         }
+        updateRadioButtonsState();
     });
 });
+userInputs.password1.addEventListener('input', () => {
+  validatePasswords();
+});
 
+userInputs.password2.addEventListener('input', () => {
+  validatePasswords();
+});
+
+function validatePasswords() {
+  const password1 = userInputs.password1.value;
+  const password2 = userInputs.password2.value;
+
+  if (!equalPasswords(password1, password2)) {
+    inputSpans.password2.innerHTML = 'Passwords do not match.';
+    userInputs.password2.style.border = outlineBorders.incorrect;
+    validationRules.password2.isValid = false;
+  } else {
+    inputSpans.password2.innerHTML = '';
+    userInputs.password2.style.border = outlineBorders.correct;
+    validationRules.password2.isValid = true;
+  }
+  updateRadioButtonsState();
+}
+
+function updateRadioButtonsState() {
+    const allFieldsValid = checkIfAllFieldsAreValid();
+
+    radioJobApplicant.disabled = !allFieldsValid;
+    radioCompany.disabled = !allFieldsValid;
+    radioCompany.checked = false;
+    radioJobApplicant.checked = false;
+    hiddenCompanyDiv.style.display = 'none';
+    hiddenJobApplicantDiv.style.display = 'none';
+}
+
+function checkIfAllFieldsAreValid() {
+    return Object.keys(validationRules).every(key => validationRules[key].isValid);
+}
 
 function isValidUsername(username) {
     return /^[a-zA-Z0-9]{8,}$/.test(username);
@@ -71,4 +121,12 @@ function isValidUsername(username) {
 
 function isValidEmail(email) {
     return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+}
+
+function isValidPassword(password) {
+    return /^[a-zA-Z0-9]{8,}$/.test(password);
+}
+
+function equalPasswords(password1, password2) {
+    return password1 === password2;
 }
