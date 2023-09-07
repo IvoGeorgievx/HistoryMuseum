7ddfd95f2e970dfcd4978e3774551050dc78c2a5
IvoGeorgievX <ivaylo.georgievxx@gmail.com>
2023-09-07 14:45:47 +0300

    adding features

--- frontend/static/js/register.js
+++ frontend/static/js/register.js
@@ -28,14 +28,36 @@ const userInputs = {
     password2: document.querySelector('input[name="password2"]'),
 }
 
+const applicantInputs = {
+    firstName: document.querySelector('input[name="first-name"]'),
+    lastName: document.querySelector('input[name="last-name"]'),
+    phoneNumber: document.querySelector('input[name="phone"]'),
+    age: document.querySelector('input[name="age"]')
+}
+
+const companyInputs = {
+    companyName: document.querySelector('input[name="company-name"]'),
+    companyPhone: document.querySelector('input[name="company-phone"]'),
+    companyAddress: document.querySelector('input[name="company-address"]'),
+    companyDescription: document.querySelector('input[name="company-description"]')
+}
+
 const inputSpans = {
     username: document.querySelector('#username-span'),
     email: document.querySelector('#email-span'),
     password1: document.querySelector('#password1'),
     password2: document.querySelector('#passwordTwo'),
+    firstName: document.querySelector('#first-name-span'),
+    lastName: document.querySelector('#last-name-span'),
+    phoneNumber: document.querySelector('#phone-span'),
+    age: document.querySelector('#age-span'),
+    companyName: document.querySelector('#company-name-span'),
+    companyPhone: document.querySelector('#company-phone-span'),
+    companyAddress: document.querySelector('#company-address-span'),
+    companyDescription: document.querySelector('#company-description-span'),
 }
 
-const validationRules = {
+const userValidationRules = {
     username: {
         validator: isValidUsername,
         message: 'Username must contain only letters and numbers and be at least 8 characters long.',
@@ -56,6 +78,44 @@ const validationRules = {
     }
 }
 
+const applicantValidationRules = {
+    firstName: {
+        validator: isValidFirstNameLastName,
+        message: 'Names must contain only letters and be at least 2 characters long.',
+    },
+    lastName: {
+        validator: isValidFirstNameLastName,
+        message: 'Names must contain only letters and be at least 2 characters long.',
+    },
+    phoneNumber: {
+        validator: isValidPhoneNumber,
+        message: 'Phone number must contain only numbers and be 10 digits long.',
+    },
+    age: {
+        validator: isValidAge,
+        message: 'You must be at least 18 years old to register.'
+    }
+}
+
+const companyValidationRules = {
+    companyName: {
+        validator: isValidCompanyName,
+        message: 'Company name must contain only letters and numbers and be at least 6 characters long.',
+    },
+    companyPhone: {
+        validator: isValidPhoneNumber,
+        message: 'Phone number must contain only numbers and be 10 digits long.'
+    },
+    companyAddress: {
+        validator: isValidAddress,
+        message: 'Please enter a valid address.'
+    },
+    companyDescription: {
+        validator: isValidDescription,
+        message: 'Please enter a short description of what does the company do.'
+    }
+}
+
 const outlineBorders = {
     correct: '4px solid green',
     incorrect: '2px solid red'
@@ -64,14 +124,14 @@ Object.keys(userInputs).map(inputName => {
     const input = userInputs[inputName];
     input.addEventListener('input', () => {
         const inputValue = input.value;
-        if (!validationRules[inputName].validator(inputValue)) {
-            inputSpans[inputName].innerHTML = validationRules[inputName].message;
+        if (!userValidationRules[inputName].validator(inputValue)) {
+            inputSpans[inputName].innerHTML = userValidationRules[inputName].message;
             input.style.border = outlineBorders.incorrect;
-            validationRules[inputName].isValid = false;
+            userValidationRules[inputName].isValid = false;
         } else {
             inputSpans[inputName].innerHTML = '';
             input.style.border = outlineBorders.correct;
-            validationRules[inputName].isValid = true;
+            userValidationRules[inputName].isValid = true;
         }
         updateRadioButtonsState();
     });
@@ -84,6 +144,34 @@ userInputs.password2.addEventListener('input', () => {
   validatePasswords();
 });
 
+Object.keys(applicantInputs).map(inputName => {
+    const input = applicantInputs[inputName];
+    input.addEventListener('input', () => {
+        const inputValue = input.value;
+        if (!applicantValidationRules[inputName].validator(inputValue)) {
+            inputSpans[inputName].innerHTML = applicantValidationRules[inputName].message;
+            input.style.border = outlineBorders.incorrect;
+        } else {
+            inputSpans[inputName].innerHTML = '';
+            input.style.border = outlineBorders.correct;
+        }
+    });
+})
+
+Object.keys(companyInputs).map(inputName => {
+    const input = companyInputs[inputName];
+    input.addEventListener('input', () => {
+        const inputValue = input.value;
+        if (!companyValidationRules[inputName].validator(inputValue)) {
+            inputSpans[inputName].innerHTML = companyValidationRules[inputName].message;
+            input.style.border = outlineBorders.incorrect;
+        } else {
+            inputSpans[inputName].innerHTML = '';
+            input.style.border = outlineBorders.correct;
+        }
+    });
+})
+
 function validatePasswords() {
   const password1 = userInputs.password1.value;
   const password2 = userInputs.password2.value;
@@ -91,11 +179,11 @@ function validatePasswords() {
   if (!equalPasswords(password1, password2)) {
     inputSpans.password2.innerHTML = 'Passwords do not match.';
     userInputs.password2.style.border = outlineBorders.incorrect;
-    validationRules.password2.isValid = false;
+    userValidationRules.password2.isValid = false;
   } else {
     inputSpans.password2.innerHTML = '';
     userInputs.password2.style.border = outlineBorders.correct;
-    validationRules.password2.isValid = true;
+    userValidationRules.password2.isValid = true;
   }
   updateRadioButtonsState();
 }
@@ -112,7 +200,7 @@ function updateRadioButtonsState() {
 }
 
 function checkIfAllFieldsAreValid() {
-    return Object.keys(validationRules).every(key => validationRules[key].isValid);
+    return Object.keys(userValidationRules).every(key => userValidationRules[key].isValid);
 }
 
 function isValidUsername(username) {
@@ -128,5 +216,27 @@ function isValidPassword(password) {
 }
 
 function equalPasswords(password1, password2) {
-    return password1 === password2 && password2 === password1;
+    return password1 === password2 && password2 === password1 && password1.length > 0 && password2.length > 0
+}
+
+function isValidFirstNameLastName(name) {
+    return /^[a-zA-Z]{2,}$/.test(name);
+}
+function isValidPhoneNumber(phoneNumber) {
+    return /^[0-9]{10}$/.test(phoneNumber);
+}
+
+function isValidAge(age) {
+    return age >= 18;
+}
+function isValidCompanyName(companyName) {
+    return /^[a-zA-Z0-9]{6,}$/.test(companyName);
+}
+
+function isValidAddress(address) {
+    return address.length > 5;
+}
+
+function isValidDescription(description) {
+    return description.length > 10;
 }
