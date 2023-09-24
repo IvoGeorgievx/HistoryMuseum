62877fbab25cfef5cdddc738e4776b6a7038b9e3
IvoGeorgievX <ivaylo.georgievxx@gmail.com>
2023-09-24 13:39:49 +0300

    adding features

--- backend/utilities/decorators.py
+++ backend/utilities/decorators.py
@@ -1,5 +1,7 @@
 from flask import request
-from werkzeug.exceptions import BadRequest
+from werkzeug.exceptions import BadRequest, Forbidden
+
+from backend.managers.auth import auth
 
 
 def validate_schema(schema_name):
@@ -11,6 +13,19 @@ def validate_schema(schema_name):
             if errors:
                 raise BadRequest(errors)
             return func(*args, **kwargs)
+
+        return wrapper
+
+    return decorated_func
+
+
+def permission_required(permission_role):
+    def decorated_func(func):
+        def wrapper(*args, **kwargs):
+            current_user = auth.current_user()
+            if current_user.role == permission_role:
+                return func(*args, **kwargs)
+            raise Forbidden("You do not have permission to do that")
 
         return wrapper
 
